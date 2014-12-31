7e90dbf01626d574dbeaa55489c9a37eb8088a1d
Deepankar Bajpeyi <dbajpeyi@gmail.com>
2014-12-31 15:58:22 +0530

    define static in express

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var app = require('express')();
+var express = require("express");
+var app = express();
 var http= require('http').Server(app);
 
 
@@ -6,6 +7,8 @@ var http= require('http').Server(app);
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html")
 })
+
+app.use(express.static(__dirname + "/public"));
 
 http.listen(3000, function(){
   console.log("Listening on port 3000");
